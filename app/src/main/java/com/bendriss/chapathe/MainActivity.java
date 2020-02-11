032f70b56f9f8b19e1b9dad3320a0665acc1174d
Tarek <tarek.bendriss@natan.fr>
2020-02-11 09:47:32 +0100

    Code refinement

--- app/src/main/java/com/bendriss/chapathe/MainActivity.java
+++ app/src/main/java/com/bendriss/chapathe/MainActivity.java
@@ -37,39 +37,16 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-       // toolbar.setSubtitle("Test Subtitle");
-       // toolbar.inflateMenu(R.menu.main_manu);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void changeStatusBarColor(Activity activity)
     {
         Window window = activity.getWindow();
-
-// clear FLAG_TRANSLUCENT_STATUS flag:
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-// finally change the color
         window.setStatusBarColor(ContextCompat.getColor(activity,R.color.colorPrimary));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.app_bar_menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.menu_settings)
-            Log.e("ID","1");
-        else
-            Log.e("ID","2");
-
-        return super.onOptionsItemSelected(item);
-    }
 }
